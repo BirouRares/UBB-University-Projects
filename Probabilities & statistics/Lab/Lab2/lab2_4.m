a390cf26a17e7ff57a0bd9e718701370b6da2ba7
BirouRares <115188710+BirouRares@users.noreply.github.com>
2024-02-29 18:27:05 +0200

    Delete Probabilities & statistics directory

--- Probabilities & statistics/Lab/Lab2/lab2_4.m
+++
@@ -1,8 +0,0 @@
-#we simulate 3 coin tosses
-
-N=input("Give the number of simulation N=");
-U=rand(3,N);
-Y=(U<0.5); #if U less then 0.5 the Y =1[heads]
-X=sum(Y);
-clf;
-hist(X);
--- Probabilities & statistics/Lab/Lab3/ex2.m
+++
@@ -1,8 +0,0 @@
-#s
-p=input("Enter P, be careful as p..:  ");
-for n=1:3:100
-  x=0:n;0.0
-  y=binopdf(x,n,p);
-  plot(x,y);
-  pause(0.5);
-  endfor
