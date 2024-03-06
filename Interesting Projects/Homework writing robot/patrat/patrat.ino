d420ddb1451db9db89c7e82cc62deb2b6c06525e
BirouRares <115188710+BirouRares@users.noreply.github.com>
2024-03-06 14:14:16 +0200

    Add files via upload

---
+++ Interesting Projects/Homework writing robot/patrat/patrat.ino
@@ -0,0 +1,40 @@
+int Index;
+int p=2;
+void setup() 
+{
+  pinMode(8, OUTPUT); //Enable
+  pinMode(5, OUTPUT); //Step
+  pinMode(6, OUTPUT);
+  pinMode(2, OUTPUT); //Direction
+  pinMode(3, OUTPUT);
+  digitalWrite(8,LOW);
+}
+
+void loop() 
+{
+  if(p==1)
+  {
+     digitalWrite(6,HIGH);
+     for(Index = 0; Index < 200000; Index++)
+      {
+         digitalWrite(3,HIGH);
+         delayMicroseconds(50);
+         digitalWrite(3,LOW);
+         delayMicroseconds(50);
+       }
+  }
+  //delay(1000);
+  if(p==2)
+  {
+    digitalWrite(5,LOW);
+
+     for(Index = 0; Index < 2000; Index++)
+      {
+        digitalWrite(2,HIGH);
+        delayMicroseconds(20);
+        digitalWrite(2,LOW);
+        delayMicroseconds(20);
+      }
+  }
+  
+}
---
+++ Interesting Projects/Homework writing robot/sketch_may09a/sketch_may09a.ino
@@ -0,0 +1,35 @@
+int Index;
+
+void setup() 
+{
+  pinMode(8, OUTPUT); //Enable
+  pinMode(5, OUTPUT); //Step
+  pinMode(2, OUTPUT); //Direction
+
+  digitalWrite(8,LOW);
+}
+
+void loop() 
+{
+  digitalWrite(5,HIGH);
+
+  for(Index = 0; Index < 2000; Index++)
+  {
+    digitalWrite(2,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(2,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+
+  digitalWrite(5,LOW);
+
+  for(Index = 0; Index < 2000; Index++)
+  {
+    digitalWrite(2,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(2,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}
