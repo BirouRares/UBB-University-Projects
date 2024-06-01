f76beab9631eee0a546457482337fd216cb7a7b8
BirouRares <gabriel.birou@yahoo.com>
2024-06-01 17:25:36 +0300

    rares face commit

---
+++ Semester IV/MPP/server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import CarRouter from './routers/car.js';
+import cors from 'cors';
+import AuthRouter from './routers/user.js';
+
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+const app = express();
+app.use(cors());
+
+db.on('error', (err) => {
+  console.log(err); 
+});
+db.once('open', () => {
+  console.log('Database Connection Established!');
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const PORT = 5000;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
+
+app.use('/', CarRouter);
+app.use('/api', AuthRouter );
